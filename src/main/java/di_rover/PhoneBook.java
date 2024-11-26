e455dd5adc80eddf3f8fe72342fc90f286f62d3f
Dmitrij Rovenskij <rovenskij@element-soft.com>
2024-11-26 21:14:07 +0300

    findByName implemented

--- src/main/java/di_rover/PhoneBook.java
+++ src/main/java/di_rover/PhoneBook.java
@@ -1,15 +1,18 @@
 package di_rover;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PhoneBook {
 
     public HashMap<Integer, String> numbers = new HashMap<Integer, String>();
+    private Map<String, Integer> reverseMap = new HashMap<>();
 
     public int add(int number, String name) {
 
         if (!numbers.containsKey(number) && !numbers.containsValue(name)) {
             numbers.put(number, name);
+            reverseMap.put(name, number);
         }
 
         return numbers.size();
@@ -20,6 +23,6 @@ public class PhoneBook {
     }
 
     public Integer findByName(String name) {
-        return null;
+        return reverseMap.get(name);
     }
 }
